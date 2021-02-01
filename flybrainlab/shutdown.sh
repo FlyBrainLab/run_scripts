f2360aea97e1b678bd3e14a22f9db1b2416bc32c
Yiyin Zhou <yiyin@ee.columbia.edu>
2021-02-01 08:26:57 -0500

    update shutdown

--- flybrainlab/shutdown.sh
+++ flybrainlab/shutdown.sh
@@ -7,8 +7,9 @@ tmux kill-session -t processor
 tmux kill-session -t nlp_hemibrain
 tmux kill-session -t nlp_flycircuit
 tmux kill-session -t nlp_l1em
+tmux kill-session -t nlp_medulla
 tmux kill-session -t neuroarch_hemibrain
 tmux kill-session -t neuroarch_flycircuit
 tmux kill-session -t neuroarch_l1em
+tmux kill-session -t neuroarch_medulla
 tmux kill-session -t neurokernel
-tmux kill-session -t fbl
