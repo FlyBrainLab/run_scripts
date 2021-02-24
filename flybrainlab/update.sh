6a40c5521af7f0fbdc8e7eb278fd4733821099e8
Yiyin Zhou <yiyin@ee.columbia.edu>
2021-02-24 02:18:37 -0500

    remove update_NeuroMynerva.sh and consolidate update.sh

--- flybrainlab/update.sh
+++ flybrainlab/update.sh
@@ -1,33 +1,40 @@
 #!/bin/bash
 
+CONDA_ROOT=$(conda info --base)
+NLP_ENV={NLP_ENV}
+FFBO_ENV={FFBO_ENV}
 FFBO_DIR={FFBO_DIR}
+
+. $CONDA_ROOT/etc/profile.d/conda.sh
+conda activate $NLP_ENV
 
 cd $FFBO_DIR/ffbo.nlp_component
 git pull
+python setup.py develop
 
-cd $FFBO_DIR/ffbo.neuroarch_nlp
-git pull
+pip install git+https://github.com/fruitflybrain/ffbo.neuroarch_nlp.git
+
+conda deactivate
+conda activate $FFBO_ENV
 
 cd $FFBO_DIR/ffbo.processor
 git pull
+python setup.py develop
 
 cd $FFBO_DIR/ffbo.neuroarch_component
 git pull
+python setup.py develop
 
-cd $FFBO_DIR/neuroarch
-git pull
-
-cd $FFBO_DIR/neurokernel
-git pull
-
-cd $FFBO_DIR/neurodriver
-git pull
+pip install --upgrade neuroarch
+pip install --upgrade neurokernel
+pip install --upgrade neurodriver
 
 cd $FFBO_DIR/retina
 git pull
+python setup.py develop
 
-cd $FFBO_DIR/Neuroballad
-git pull
+pip install git+https://github.com/flybrainlab/Neuroballad.git
+pip install --upgrade flybrainlab
+pip install --upgrade neuromynerva
 
-cd $FFBO_DIR/FBLClient
-git pull
+conda deactivate
