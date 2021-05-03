621e15a56e38c614e8c0ef867968c7492b60b9d7
Yiyin Zhou <yiyin@ee.columbia.edu>
2021-05-02 21:53:59 -0400

    update script for new crossbar env

--- flybrainlab/update.sh
+++ flybrainlab/update.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 CONDA_ROOT=$(conda info --base)
+CROSSBAR_ENV={CROSSBAR_ENV}
 NLP_ENV={NLP_ENV}
 FFBO_ENV={FFBO_ENV}
 FFBO_DIR={FFBO_DIR}
@@ -12,29 +13,26 @@ cd $FFBO_DIR/ffbo.nlp_component
 git pull
 python setup.py develop
 
-pip install git+https://github.com/fruitflybrain/ffbo.neuroarch_nlp.git
+pip install --upgrade git+https://github.com/fruitflybrain/ffbo.neuroarch_nlp.git
 
 conda deactivate
-conda activate $FFBO_ENV
+conda activate $CROSSBAR_ENV
 
 cd $FFBO_DIR/ffbo.processor
 git pull
 python setup.py develop
 
+conda deactivate
+conda activate $FFBO_ENV
+
 cd $FFBO_DIR/ffbo.neuroarch_component
 git pull
 python setup.py develop
 
-pip install --upgrade neuroarch
-pip install --upgrade neurokernel
-pip install --upgrade neurodriver
-
-cd $FFBO_DIR/retina
+cd $FFBO_DIR/ffbo.neurokernel_component
 git pull
 python setup.py develop
 
-pip install git+https://github.com/flybrainlab/Neuroballad.git
-pip install --upgrade flybrainlab
-pip install --upgrade neuromynerva
+pip install --upgrade neuroarch neurokernel neurodriver git+https://github.com/flybrainlab/Neuroballad.git flybrainlab neuromynerva
 
 conda deactivate
