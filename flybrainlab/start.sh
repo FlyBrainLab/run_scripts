27951510d4ea9fb9c8b86abe7f19d0bdc7930738
Yiyin Zhou <yiyin@ee.columbia.edu>
2021-05-12 13:04:54 -0400

    fix start.sh

--- flybrainlab/start.sh
+++ flybrainlab/start.sh
@@ -10,26 +10,26 @@ tmux new-session -d -s "processor" $FFBO_DIR/bin/run_processor.sh
 sleep 20
 if [ -d "$ORIENTDB_ROOT/databases/hemibrain" ]
 then
-    tmux new-session -d -s "nlp_hemibrain" $FFBO_DIR/bin/run_nlp.sh -b hemibrain
-    tmux new-session -d -s "neuroarch_hemibrain" $FFBO_DIR/bin/run_neuroarch.sh -a hemibrain
+    tmux new-session -d -s "nlp_hemibrain" $FFBO_DIR/bin/run_nlp.sh -a hemibrain
+    tmux new-session -d -s "neuroarch_hemibrain" $FFBO_DIR/bin/run_neuroarch.sh -b hemibrain
 fi
 
 if [ -d "$ORIENTDB_ROOT/databases/flycircuit" ]
 then
-    tmux new-session -d -s "nlp_flycircuit" $FFBO_DIR/bin/run_nlp.sh -b flycircuit
-    tmux new-session -d -s "neuroarch_flycircuit" $FFBO_DIR/bin/run_neuroarch.sh -a flycircuit
+    tmux new-session -d -s "nlp_flycircuit" $FFBO_DIR/bin/run_nlp.sh -a flycircuit
+    tmux new-session -d -s "neuroarch_flycircuit" $FFBO_DIR/bin/run_neuroarch.sh -b flycircuit
 fi
 
 if [ -d "$ORIENTDB_ROOT/databases/l1em" ]
 then
-    tmux new-session -d -s "nlp_l1em" $FFBO_DIR/bin/run_nlp.sh -b l1em
-    tmux new-session -d -s "neuroarch_l1em" $FFBO_DIR/bin/run_neuroarch.sh -a l1em
+    tmux new-session -d -s "nlp_l1em" $FFBO_DIR/bin/run_nlp.sh -a l1em
+    tmux new-session -d -s "neuroarch_l1em" $FFBO_DIR/bin/run_neuroarch.sh -b l1em
 fi
 
 if [ -d "$ORIENTDB_ROOT/databases/medulla" ]
 then
-    tmux new-session -d -s "nlp_medulla" $FFBO_DIR/bin/run_nlp.sh -b medulla
-    tmux new-session -d -s "neuroarch_medulla" $FFBO_DIR/bin/run_neuroarch.sh -a medulla
+    tmux new-session -d -s "nlp_medulla" $FFBO_DIR/bin/run_nlp.sh -a medulla
+    tmux new-session -d -s "neuroarch_medulla" $FFBO_DIR/bin/run_neuroarch.sh -b medulla
 fi
 
 tmux new-session -d -s "neurokernel" $FFBO_DIR/bin/run_neurokernel.sh
