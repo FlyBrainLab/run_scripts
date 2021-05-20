477533dcda878d0daa9818c451bd140a5b69407a
Yiyin Zhou <yiyin@ee.columbia.edu>
2021-05-20 13:30:24 -0400

    update start.sh

--- neuronlp/start.sh
+++ neuronlp/start.sh
@@ -5,9 +5,9 @@ FFBO_DIR={FFBO_DIR}
 tmux new-session -d -s "db" $FFBO_DIR/bin/run_database.sh
 
 tmux new-session -d -s "processor" $FFBO_DIR/bin/run_processor.sh
-tmux new-session -d -s "nlp" $FFBO_DIR/bin/run_nlp.sh -a $1
+tmux new-session -d -s "nlp" "$FFBO_DIR/bin/run_nlp.sh -a $1"
 sleep 20
-tmux new-session -d -s "neuroarch" $FFBO_DIR/bin/run_neuroarch.sh -b $1
+tmux new-session -d -s "neuroarch" "$FFBO_DIR/bin/run_neuroarch.sh -b $1"
 
 echo "Servers started. Check tmux sessions for status. To shutdown, run ~/ffbo/bin/shutdown.sh"
 
