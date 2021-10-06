2f14e2412362de22c0abe3ec55f7e5c7f3ef686f
Yiyin Zhou <yiyin@ee.columbia.edu>
2021-10-06 06:15:42 -0400

    add environment to force MPI to communicate using lo in case docker is installed that prevent neurokernel from running

--- flybrainlab/run_neurokernel.sh
+++ flybrainlab/run_neurokernel.sh
@@ -6,4 +6,5 @@ FFBO_DIR={FFBO_DIR}
 . $CONDA_ROOT/etc/profile.d/conda.sh
 conda activate $FFBO_ENV
 cd $FFBO_DIR/nk_tmp
+export OMPI_MCA_btl_tcp_if_include=lo
 python $FFBO_DIR/ffbo.neurokernel_component/neurokernel_component/neurokernel_component.py
