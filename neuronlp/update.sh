edb0265e6b720164ba0eb1067a13b7319cf9f12c
Yiyin Zhou <yiyin@ee.columbia.edu>
2021-10-25 15:45:50 -0400

    update update.sh

--- neuronlp/update.sh
+++ neuronlp/update.sh
@@ -3,20 +3,11 @@
 
 CONDA_ROOT=$(conda info --base)
 CROSSBAR_ENV={CROSSBAR_ENV}
-NLP_ENV={NLP_ENV}
 FFBO_ENV={FFBO_ENV}
 FFBO_DIR={FFBO_DIR}
 
 . $CONDA_ROOT/etc/profile.d/conda.sh
-conda activate $NLP_ENV
 
-python -m pip install --upgrade git+https://github.com/fruitflybrain/ffbo.neuroarch_nlp.git
-
-cd $FFBO_DIR/ffbo.nlp_component
-git pull
-python -m pip install -e .
-
-conda deactivate
 conda activate $CROSSBAR_ENV
 
 cd $FFBO_DIR/ffbo.processor
@@ -25,6 +16,12 @@ python -m pip install -e .
 
 conda deactivate
 conda activate $FFBO_ENV
+
+python -m pip install --upgrade git+https://github.com/fruitflybrain/ffbo.neuroarch_nlp.git git+https://github.com/fruitflybrain/quepy.git
+
+cd $FFBO_DIR/ffbo.nlp_component
+git pull
+python -m pip install -e .
 
 cd $FFBO_DIR/ffbo.neuroarch_component
 git pull
