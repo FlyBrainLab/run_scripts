f7866e24c5adcebdfb211c9de48469dceb730214
Yiyin Zhou <yiyin@ee.columbia.edu>
2021-10-25 15:47:29 -0400

    update shutdown.sh

--- neuronlp/shutdown.sh
+++ neuronlp/shutdown.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 ORIENTDB_ROOT={ORIENTDB_ROOT}
-$ORIENTDB_ROOT/bin/shutdown.sh
+BINARY_PORT=$(awk -F "=" '/binary-port/ {print $2}' ~/.ffbo/config/config.ini | tr -d ' ')
+if [ -z "$BINARY_PORT" ];
+then
+    $ORIENTDB_ROOT/bin/shutdown.sh -u root -p root;
+else
+    $ORIENTDB_ROOT/bin/shutdown.sh -P $BINARY_PORT -u root -p root;
+fi
 tmux kill-session -t db
 tmux kill-session -t processor
 tmux kill-session -t nlp
