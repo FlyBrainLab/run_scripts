e14a449464cf0d5d227151ae3cceed565c4b246b
Yiyin Zhou <yiyin.zhou@fordham.edu>
2024-06-15 19:24:24 -0400

    add additional datasets in start.sh

--- flybrainlab/start.sh
+++ flybrainlab/start.sh
@@ -32,6 +32,25 @@ then
     tmux new-session -d -s "neuroarch_medulla" "$FFBO_DIR/bin/run_neuroarch.sh -b medulla"
 fi
 
+if [ -d "$ORIENTDB_ROOT/databases/manc" ]
+then
+    tmux new-session -d -s "nlp_manc" "$FFBO_DIR/bin/run_nlp.sh -a manc"
+    tmux new-session -d -s "neuroarch_manc" "$FFBO_DIR/bin/run_neuroarch.sh -b manc"
+fi
+
+if [ -d "$ORIENTDB_ROOT/databases/flywire" ]
+then
+    tmux new-session -d -s "nlp_flywire" "$FFBO_DIR/bin/run_nlp.sh -a flywire"
+    tmux new-session -d -s "neuroarch_flywire" "$FFBO_DIR/bin/run_neuroarch.sh -b flywire"
+fi
+
+if [ -d "$ORIENTDB_ROOT/databases/fib19" ]
+then
+    tmux new-session -d -s "nlp_fib19" "$FFBO_DIR/bin/run_nlp.sh -a fib19"
+    tmux new-session -d -s "neuroarch_fib19" "$FFBO_DIR/bin/run_neuroarch.sh -b fib19"
+fi
+
+
 tmux new-session -d -s "neurokernel" $FFBO_DIR/bin/run_neurokernel.sh
 
 echo "Servers started. Check tmux sessions for status. To shutdown, run $FFBO_DIR/bin/shutdown.sh"
